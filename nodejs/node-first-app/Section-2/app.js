8ef161d45854ad38eabe4c4c8481ba5aa072a360
Lee Jones <leejones111@hotmail.co.uk>
2021-08-15 17:32:53 +0100

    added an err function

--- nodejs/node-first-app/Section-2/app.js
+++ nodejs/node-first-app/Section-2/app.js
@@ -11,6 +11,7 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
+    
     if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
@@ -18,16 +19,17 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
         });
 
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
