ce6028916193ad21d98535654ab0740d7ff864a2
Lee Jones <leejones111@hotmail.co.uk>
2021-08-21 21:24:32 +0100

    added 2 more routes

--- nodejs/node-first-app/app.js
+++ nodejs/node-first-app/app.js
@@ -4,13 +4,27 @@ const http = require('http');
 const express = require('express');
 // create express application set to app by running express as a function
 const app = express();
-// use method 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // next is a function that allows the request to continue to the next middleware in line
+// // use method 
+// app.use((req, res, next) => {
+//     console.log('In the middleware!');
+//     next(); // next is a function that allows the request to continue to the next middleware in line
+// });
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    console.log('Another middleware');
+    res.send('<html><body><form action="/product" method="POST"><input tyoe="text" name="title"><button type="submit">Add Product</button></form></body></html>')
 });
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express</h1>'); // allows us to send a response with a body if we choose
 });
