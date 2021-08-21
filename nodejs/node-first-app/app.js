a05315f110cd237e9e220e1eed6e9fb56d027e5d
Lee Jones <leejones111@hotmail.co.uk>
2021-08-21 21:49:54 +0100

    seperated the routes into seperate files

--- nodejs/node-first-app/app.js
+++ nodejs/node-first-app/app.js
@@ -1,34 +1,20 @@
-// core modules
-const http = require('http');
 // third-party dependency
 const express = require('express');
+const bodyParser = require('body-parser');
 // create express application set to app by running express as a function
 const app = express();
-// // use method 
-// app.use((req, res, next) => {
-//     console.log('In the middleware!');
-//     next(); // next is a function that allows the request to continue to the next middleware in line
-// });
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded());
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 app.use('/', (req, res, next) => {
     console.log('This always runs!');
     next();
 })
-
-app.use('/add-product', (req, res, next) => {
-    console.log('Another middleware');
-    res.send('<html><body><form action="/product" method="POST"><input tyoe="text" name="title"><button type="submit">Add Product</button></form></body></html>')
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express</h1>'); // allows us to send a response with a body if we choose
-});
-
-// app is a valid request handler so can be passed to createServer
 
 app.listen(3000);
---
+++ nodejs/node-first-app/routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express</h1>'); // allows us to send a response with a body if we choose
+});
+
+module.exports = router;
