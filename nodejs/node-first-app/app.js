7c61b7ac97783bb1ba14c906b1f836f343aff840
Lee Jones <leejones111@hotmail.co.uk>
2021-08-26 23:55:55 +0100

    re-routed from app.js

--- nodejs/node-first-app/app.js
+++ nodejs/node-first-app/app.js
@@ -9,17 +9,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// add a catchall middleware
+// add a catchall route
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>')
 });
-
-// app.use('/', (req, res, next) => {
-//     console.log('This always runs!');
-//     next();
-// })
 
 app.listen(3000);
--- nodejs/node-first-app/routes/admin.js
+++ nodejs/node-first-app/routes/admin.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 router.use('/add-product', (req, res, next) => {
     console.log('Another middleware');
-    res.send('<form action="/product" method="POST"><input tyoe="text" name="title"><button type="submit">Add Product</button></form>')
+    res.send('<form action="/add-product" method="POST"><input tyoe="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
