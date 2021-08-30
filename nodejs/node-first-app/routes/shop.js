c9b7b324a4c03f327789f26ae638cc64236b63a1
Lee Jones <leejones111@hotmail.co.uk>
2021-08-30 21:25:46 +0100

    added shop.html to the router

--- nodejs/node-first-app/routes/shop.js
+++ nodejs/node-first-app/routes/shop.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/',(req, res, next) => {
     // console.log('In another middleware!');
     // res.send('<h1>Hello from Express</h1>'); // allows us to send a response with a body if we choose
-    res.sendFile(path.join(__dirname, 'views', 'shop.html'));
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
