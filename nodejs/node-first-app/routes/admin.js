6ab256aaf5265d03db834e3bcd15bdc1249d46ff
Lee Jones <leejones111@hotmail.co.uk>
2021-08-30 21:35:24 +0100

    added admin add-product.html to the router

--- nodejs/node-first-app/routes/admin.js
+++ nodejs/node-first-app/routes/admin.js
@@ -1,11 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-product => GET 
-router.use('/add-product', (req, res, next) => {
-    console.log('Another middleware');
-    res.send('<form action="/add-product" method="POST"><input tyoe="text" name="title"><button type="submit">Add Product</button></form>')
+router.get('/add-product', (req, res, next) => {
+    // console.log('Another middleware');
+    // res.send('<form action="/add-product" method="POST"><input tyoe="text" name="title"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST 
