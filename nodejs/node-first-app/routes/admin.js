ca7b8a88c97ea6a05e2d1646ab0e8fd40264dc94
Lee Jones <leejones111@hotmail.co.uk>
2021-09-03 23:15:57 +0100

    tidyness

--- nodejs/node-first-app/routes/admin.js
+++ nodejs/node-first-app/routes/admin.js
@@ -6,8 +6,6 @@ const router = express.Router();
 
 // /admin/add-product => GET 
 router.get('/add-product', (req, res, next) => {
-    // console.log('Another middleware');
-    // res.send('<form action="/add-product" method="POST"><input tyoe="text" name="title"><button type="submit">Add Product</button></form>')
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
--- nodejs/node-first-app/routes/shop.js
+++ nodejs/node-first-app/routes/shop.js
@@ -5,8 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    // console.log('In another middleware!');
-    // res.send('<h1>Hello from Express</h1>'); // allows us to send a response with a body if we choose
     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
