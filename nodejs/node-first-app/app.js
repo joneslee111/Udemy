3ee78b2bc8b12e4e2fd4efd257a6e5e41b4eeae0
Lee Jones <leejones111@hotmail.co.uk>
2021-09-07 00:21:31 +0100

    added the route to app.js

--- nodejs/node-first-app/app.js
+++ nodejs/node-first-app/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded());
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// add a catch all route
+// add a catch all routes
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
