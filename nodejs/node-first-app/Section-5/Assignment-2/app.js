69a6651f03a41595644eb19c65c3dae58948121f
Lee Jones <leejones111@hotmail.co.uk>
2021-09-08 01:46:35 +0100

    added js served statically

--- nodejs/node-first-app/Section-5/Assignment-2/app.js
+++ nodejs/node-first-app/Section-5/Assignment-2/app.js
@@ -7,6 +7,8 @@ const app = express();
 const homeRoutes = require('./routes/home');
 const userRoutes = require('./routes/users');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(homeRoutes);
 app.use(userRoutes);
 
